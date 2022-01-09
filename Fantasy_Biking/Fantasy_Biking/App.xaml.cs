fa70a81c1451942c118f4ac2dc285ca9b95449b0
anorak6435 <stefan.beckers14@gmail.com>
2022-01-10 00:48:07 +0100

    Single page app met database setup in activities

--- Fantasy_Biking/Fantasy_Biking/App.xaml.cs
+++ Fantasy_Biking/Fantasy_Biking/App.xaml.cs
@@ -6,11 +6,19 @@ namespace Fantasy_Biking
 {
     public partial class App : Application
     {
+        public static string DatabaseLocation = string.Empty;
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+            DatabaseLocation = databaseLocation;
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()

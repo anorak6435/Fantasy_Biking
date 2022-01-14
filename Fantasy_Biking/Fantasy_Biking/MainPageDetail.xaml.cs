ddb643c435851441fd9d61d817269a53724c06f6
Wodanson <1782819gillissen@zuyd.nl>
2022-01-14 12:24:37 +0100

    edit to mainpage

--- Fantasy_Biking/Fantasy_Biking/MainPageDetail.xaml.cs
+++ Fantasy_Biking/Fantasy_Biking/MainPageDetail.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Fantasy_Biking
 {
-    public partial class MainPage : FlyoutPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageDetail : ContentPage
     {
-        public MainPage()
+        public MainPageDetail()
         {
             InitializeComponent();
         }

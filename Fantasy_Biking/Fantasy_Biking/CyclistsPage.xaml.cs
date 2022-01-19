c938b341220172d25d45dd5fda080158cf15e429
Wodanson <1782819gillissen@zuyd.nl>
2022-01-19 13:13:32 +0100

    Made a Cyclist Page

---
+++ Fantasy_Biking/Fantasy_Biking/CyclistsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Fantasy_Biking
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CyclistsPage : ContentPage
+    {
+        public CyclistsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

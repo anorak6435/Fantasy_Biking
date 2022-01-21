78d4ebaed3c46131f7365aa493af5e96ad2ec0d8
anorak6435 <stefan.beckers14@gmail.com>
2022-01-21 17:41:49 +0100

    Add Note model to remember user data

---
+++ Fantasy_Biking/Fantasy_Biking/Model/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fantasy_Biking.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Contents { get; set; }
+    }
+}

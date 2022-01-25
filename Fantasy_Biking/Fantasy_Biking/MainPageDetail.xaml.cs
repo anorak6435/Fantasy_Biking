6f98e7d2efb1f2228b34cb73a7952ad48ec590c8
anorak6435 <stefan.beckers14@gmail.com>
2022-01-25 17:05:02 +0100

    Show teammembers on MainPageDetail xaml

--- Fantasy_Biking/Fantasy_Biking/MainPageDetail.xaml.cs
+++ Fantasy_Biking/Fantasy_Biking/MainPageDetail.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fantasy_Biking.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace Fantasy_Biking
         public MainPageDetail()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            MyTeam_List.ItemsSource = TeamLogic.GetMyTeam();
         }
     }
 }

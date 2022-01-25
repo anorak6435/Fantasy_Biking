60bfc4cb934f837ce58e0244d2a69ceb78ffa9cc
Wodanson <1782819gillissen@zuyd.nl>
2022-01-25 20:43:46 +0100

    Edit password and username added

--- Fantasy_Biking/Fantasy_Biking/Userpage.xaml.cs
+++ Fantasy_Biking/Fantasy_Biking/Userpage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Fantasy_Biking.Model;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +19,65 @@ namespace Fantasy_Biking
             InitializeComponent();
             UsernameLabel.Text = MainPage.loggedInUser.Name;
             PasswordLabel.Text = MainPage.loggedInUser.Password;
+        }
+
+        private void Edit_Username_Clicked(object sender, EventArgs e)
+        {
+            ConfirmUsernameEdit.IsVisible = true;
+            UsernameBodyEntry.IsVisible = true;
+            Edit_Username.IsVisible = false;
+        }
+        private void Edit_password_Clicked(object sender, EventArgs e)
+        {
+            ConfirmPasswordEdit.IsVisible = true;
+            PasswordBodyEntry.IsVisible = true;
+            Edit_password.IsVisible = false;
+        }
+
+        private void ConfirmUsernameEdit_Clicked(object sender, EventArgs e)
+        {
+            int updatedRows;
+            MainPage.loggedInUser.Name = UsernameBodyEntry.Text;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation))
+            {
+                sQLiteConnection.CreateTable<Biker>();
+                updatedRows = sQLiteConnection.Update(MainPage.loggedInUser);
+            }
+            if (updatedRows > 0)
+            {
+                _ = DisplayAlert("Edit", "Your Username has been edited", "Ok");
+                ConfirmUsernameEdit.IsVisible = false;
+                UsernameBodyEntry.IsVisible = false;
+                Edit_Username.IsVisible = true;
+                UsernameLabel.Text = MainPage.loggedInUser.Name;
+            }
+            else
+            {
+                _ = DisplayAlert("Failed", "Your Question has not been edited", "Ok");
+            }
+        }
+
+        private void ConfirmPasswordEdit_Clicked(object sender, EventArgs e)
+        {
+            int updatedRows;
+            MainPage.loggedInUser.Password = PasswordBodyEntry.Text;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation))
+            {
+                sQLiteConnection.CreateTable<Biker>();
+                updatedRows = sQLiteConnection.Update(MainPage.loggedInUser);
+            }
+            if (updatedRows > 0)
+            {
+                _ = DisplayAlert("Edit", "Your Password has been edited", "Ok");
+                ConfirmPasswordEdit.IsVisible = false;
+                PasswordBodyEntry.IsVisible = false;
+                Edit_password.IsVisible = true;
+                PasswordLabel.Text = MainPage.loggedInUser.Password;
+            }
+            else
+            {
+                _ = DisplayAlert("Failed", "Your Password has not been edited", "Ok");
+            }
         }
     }
 }

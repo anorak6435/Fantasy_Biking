7f1faf5ca59f8734e16af531054ac6eb9035ff6a
anorak6435 <stefan.beckers14@gmail.com>
2022-01-26 12:45:34 +0100

    Add camera to take profile pictures

--- Fantasy_Biking/Fantasy_Biking/RegisterPage.xaml.cs
+++ Fantasy_Biking/Fantasy_Biking/RegisterPage.xaml.cs
@@ -2,6 +2,7 @@
 using Fantasy_Biking.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +33,49 @@ namespace Fantasy_Biking
             }
         }
 
+
         private async void NewProfileImage_Clicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Option: From where get the image?", "Cancel", null, "Camera", "Gallery");
+            switch (action)
+            {
+                case "Camera":
+                    await PicFromCamera();
+                    break;
+                case "Gallery":
+                    PicFromGallery();
+                    break;
+            }
+        }
+
+        private async Task PicFromCamera()
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+            await LoadPhotoAsync(photo);
+            if (UserPicturePath != string.Empty)
+            {
+                UserPic.Source = UserPicturePath; // Update the picture on the page
+            }   
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                UserPicturePath = string.Empty;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            UserPicturePath = newFile;
+        }
+
+        private async void PicFromGallery()
         {
             var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
             {

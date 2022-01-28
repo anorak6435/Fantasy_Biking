18a52964fb7a8061045c793b12daee506c857897
anorak6435 <stefan.beckers14@gmail.com>
2022-01-28 11:08:50 +0100

    Move Note database logic into the NotesLogic file.

--- Fantasy_Biking/Fantasy_Biking/Logic/NotesLogic.cs
+++ Fantasy_Biking/Fantasy_Biking/Logic/NotesLogic.cs
@@ -9,7 +9,49 @@ namespace Fantasy_Biking.Logic
 {
     public class NotesLogic
     {
+        public static int DeleteNote(object note)
+        {
+            int deletedRows;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation))
+            {
+                sQLiteConnection.CreateTable<BikerNote>();
+                sQLiteConnection.CreateTable<LeagueNote>();
+                deletedRows = sQLiteConnection.Delete(note);
+            }
+            return deletedRows;
+        }
+        public static void UpdateLeagueNote(LeagueNote current_race)
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation);
+            sQLiteConnection.CreateTable<LeagueNote>();
+            sQLiteConnection.Update(current_race);
+            sQLiteConnection.Close();
+        }
 
+        public static void UpdateBikerNote(BikerNote current_biker)
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation);
+            sQLiteConnection.CreateTable<BikerNote>();
+            sQLiteConnection.Update(current_biker);
+            sQLiteConnection.Close();
+        }
+
+        public static void InsertBikerNote(BikerNote note)
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation);
+            sQLiteConnection.CreateTable<BikerNote>();
+            int insertedRows = sQLiteConnection.Insert(note);
+            sQLiteConnection.Close();
+        }
+
+        public static void InsertLeagueNote(LeagueNote note)
+        {
+            // open connection to insert note into the database
+            SQLiteConnection sQLiteConnection = new SQLiteConnection(App.DatabaseLocation);
+            sQLiteConnection.CreateTable<LeagueNote>();
+            int insertedRows = sQLiteConnection.Insert(note);
+            sQLiteConnection.Close();
+        }
 
         public static List<object> GetAllNotes()
         {
